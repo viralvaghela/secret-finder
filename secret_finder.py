c56fab929fba69bb922f88aa2f71dfdccdd0d1a3
viralvaghela <viralvaghela7109@gmail.com>
2023-12-01 20:17:29 +0530

    fixed #1

--- secret_finder.py
+++ secret_finder.py
@@ -11,9 +11,12 @@ init(autoreset=True)
 APKTOOL_PATH = 'apktool_2.7.0.jar'
 
 # Additional keywords to find sensitive hardcoded values
-SENSITIVE_KEYWORDS = [' token ','TOKEN','TOKENS', 'tokens',' key ', 'password', 'secret', 'SECRET', 'confidential','_key','_token','api_key']
+SENSITIVE_KEYWORDS = [' token ', 'TOKEN', 'TOKENS', 'tokens', ' key ',
+                      'password', 'secret', 'SECRET', 'confidential', '_key', '_token', 'api_key']
 
 # Decorator to print tool name with ASCII art
+
+
 def print_tool_name(func):
     def wrapper(*args, **kwargs):
         print(r'''
@@ -36,13 +39,27 @@ def print_tool_name(func):
 
 # Decompiles the specified APK file
 # Decompiles the specified APK file
-def decompile_apk(apk_path):
-    decompiled_path = apk_path + '_decompiled'
 
-    print(f"Decompiling APK: {apk_path} It may take some time, please wait...\n ")
+
+def decompile_apk(apk_path):
+    # Remove double quotes from the APK path
+    apk_path = apk_path.strip('"')
+
+    # Extract the APK file name without extension
+    apk_name = os.path.splitext(os.path.basename(apk_path))[0]
+
+    # Replace spaces with underscores in the APK name
+    apk_name = apk_name.replace(' ', '_')
+
+    # Create a new directory in the current working directory
+    decompiled_path = os.path.join(os.getcwd(), apk_name + '_decompiled')
+
+    print(
+        f"Decompiling APK: {apk_path} It may take some time, please wait...\n ")
 
     # Start the decompilation process
-    process = subprocess.Popen(['java', '-jar', APKTOOL_PATH, 'd', apk_path, '-o', decompiled_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    process = subprocess.Popen(['java', '-jar', APKTOOL_PATH, 'd', apk_path, '-o', decompiled_path],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
     pbar = None
 
@@ -53,7 +70,8 @@ def decompile_apk(apk_path):
             if match:
                 progress = int(match.group(1))
                 if not pbar:
-                    pbar = tqdm(total=100, desc="Decompiling", unit="%", leave=True)
+                    pbar = tqdm(total=100, desc="Decompiling",
+                                unit="%", leave=True)
                 pbar.n = progress
                 pbar.refresh()
         else:
@@ -91,6 +109,8 @@ def check_file_for_sensitive_keywords(file_path):
     return matches
 
 # Checks the APK for matching sensitive keywords in specified file(s) or all files
+
+
 def check_apk_for_sensitive_keywords(apk_path, check_all_files=False):
     decompiled_path = decompile_apk(apk_path)
 
@@ -110,14 +130,18 @@ def check_apk_for_sensitive_keywords(apk_path, check_all_files=False):
                 for file in files:
                     if file.endswith(".smali"):  # Only process *.smali files
                         file_path = os.path.join(root, file)
-                        file_matches = check_file_for_sensitive_keywords(file_path)
+                        file_matches = check_file_for_sensitive_keywords(
+                            file_path)
                         if file_matches:
                             matches.extend(file_matches)
                         pbar.update(1)
-                        time.sleep(0.01)  # Optional delay for smoother progress display
+                        # Optional delay for smoother progress display
+                        time.sleep(0.01)
     else:
-        strings_xml_path = os.path.join(decompiled_path, 'res', 'values', 'strings.xml')
-        manifest_xml_path = os.path.join(decompiled_path, 'AndroidManifest.xml')
+        strings_xml_path = os.path.join(
+            decompiled_path, 'res', 'values', 'strings.xml')
+        manifest_xml_path = os.path.join(
+            decompiled_path, 'AndroidManifest.xml')
 
         total_files = 2
         pbar = tqdm(total=total_files, desc="Progress", unit="file")
@@ -128,20 +152,24 @@ def check_apk_for_sensitive_keywords(apk_path, check_all_files=False):
         matches.extend(strings_matches)
         matches.extend(manifest_matches)
         pbar.update(total_files)
-    
+
     return matches
 
 # Main function
+
+
 @print_tool_name
 def main():
     apk_path = input("Enter the path to the APK file: ")
 
-    file_check_option = int(input("Select file check option \n1. Basic Scan(Fast - Check for only important)\n2. Advance Scan(Slow - Check for All files): "))
+    file_check_option = int(input(
+        "Select file check option \n1. Basic Scan(Fast - Check for only important)\n2. Advance Scan(Slow - Check for All files): "))
 
     if file_check_option == 1:
         sensitive_matches = check_apk_for_sensitive_keywords(apk_path)
     elif file_check_option == 2:
-        sensitive_matches = check_apk_for_sensitive_keywords(apk_path, check_all_files=True)
+        sensitive_matches = check_apk_for_sensitive_keywords(
+            apk_path, check_all_files=True)
     else:
         print("Invalid option selected. Please try again.")
         return
@@ -154,6 +182,7 @@ def main():
             print(Fore.GREEN + f"File: {match[0]}")
             print(Fore.GREEN + f"Line Number: {match[1]}")
             print(Fore.GREEN + f"Line: {match[2]}\n")
+
 
 if __name__ == '__main__':
     main()
